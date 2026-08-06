8aecd25877c381c1187dff647afe7f390ce3e67e
PRIOR <Stephen.PRIOR@EDUCATION.GOV.UK>
2026-08-06 11:46:40 +0100

    complete imported application service

--- src/GovUK.Dfe.FlexForms.Web/Services/ImportedApplicationService.cs
+++ src/GovUK.Dfe.FlexForms.Web/Services/ImportedApplicationService.cs
@@ -1,14 +1,46 @@
-﻿using GovUK.Dfe.FlexForms.Api.Client.Contracts;
+﻿using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Request;
+using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
+using GovUK.Dfe.FlexForms.Api.Client.Contracts;
 using GovUK.Dfe.FlexForms.Web.Interfaces;
+using System.Diagnostics;
+using System.Text;
+using System.Text.Json;
 
 namespace GovUK.Dfe.FlexForms.Web.Services
 {
     public class ImportedApplicationService(IApplicationsClient client) : IImportedApplicationService
     {
-        public Task<bool> SaveApplicationAsync(string reference, IDictionary<string, object> data)
+        private readonly JsonSerializerOptions jsonSerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
+
+        public async Task<bool> SaveApplicationAsync(string reference, IDictionary<string, object> data)
         {
-            // TODO call the API client to create the application, add the response, and submit the application
-            return Task.FromResult(false);
+            // TODO use logger instead of Debug.WriteLine
+
+            ApplicationDto createdApplication = await client.CreateApplicationAsync(new CreateApplicationRequest    
+            {
+                TemplateId = Guid.Parse("00000000-0000-0000-0000-000000000001"),
+                InitialResponseBody = "{}"
+            });
+            Debug.WriteLine($"Created application {createdApplication.ApplicationReference} with ID {createdApplication.ApplicationId}");
+
+            Dictionary<string, object> fields = [];
+            foreach (var kvp in data)
+            {
+                KeyValuePair<string, object> field = new(kvp.Key, new { kvp.Value, Completed = true });
+                fields.Add(field.Key, field.Value);
+            }
+            string responseBody = JsonSerializer.Serialize(fields, jsonSerializerOptions);
+            string encodedResponse = Convert.ToBase64String(Encoding.UTF8.GetBytes(responseBody));
+            ApplicationResponseDto applicationResponse = await client.AddApplicationResponseAsync(createdApplication.ApplicationId, new AddApplicationResponseRequest
+            {
+                ResponseBody = encodedResponse
+            });
+            Debug.WriteLine($"Added application response {applicationResponse.ApplicationId} for application {createdApplication.ApplicationReference}");
+
+            ApplicationDto submittedApplication = await client.SubmitApplicationAsync(createdApplication.ApplicationId);
+            Debug.WriteLine($"Submitted application {submittedApplication.ApplicationReference} with status {submittedApplication.Status}");
+
+            return true;
         }
     }
 }
--- src/Tests/GovUK.Dfe.FlexForms.Web.UnitTests/Services/ImportedApplicationServiceTest.cs
+++ src/Tests/GovUK.Dfe.FlexForms.Web.UnitTests/Services/ImportedApplicationServiceTest.cs
@@ -1,8 +1,12 @@
-﻿using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Request;
+﻿using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Enums;
+using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Request;
 using GovUK.Dfe.CoreLibs.Contracts.ExternalApplications.Models.Response;
 using GovUK.Dfe.FlexForms.Api.Client.Contracts;
 using GovUK.Dfe.FlexForms.Web.Services;
 using Moq;
+using System.Text;
+using System.Text.Json;
+using Xunit.Abstractions;
 
 namespace GovUK.Dfe.FlexForms.Web.UnitTests.Services
 {
@@ -12,11 +16,14 @@ namespace GovUK.Dfe.FlexForms.Web.UnitTests.Services
         private static readonly Guid TemplateId = Guid.Parse("00000000-0000-0000-0000-000000000001");
         private readonly Mock<IApplicationsClient> mockClient;
         private readonly ImportedApplicationService service;
+        private readonly JsonSerializerOptions jsonSerializerOptions = new() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase, WriteIndented = true };
+        private readonly ITestOutputHelper output;
 
-        public ImportedApplicationServiceTest()
+        public ImportedApplicationServiceTest(ITestOutputHelper output)
         {
             mockClient = new Mock<IApplicationsClient>();
             service = new ImportedApplicationService(mockClient.Object);
+            this.output = output;
         }
 
         [Fact]
@@ -27,21 +34,29 @@ namespace GovUK.Dfe.FlexForms.Web.UnitTests.Services
             ApplicationDto createdApplication = new()
             {
                 ApplicationId = Guid.Parse("00000000-0000-0000-0000-000000000002"),
-                ApplicationReference = ApplicationReference
+                ApplicationReference = ApplicationReference,
+                Status = ApplicationStatus.Created
             };
             mockClient.Setup(c => c.CreateApplicationAsync(It.IsAny<CreateApplicationRequest>()))
                 .Callback<CreateApplicationRequest, CancellationToken>((request, cancellationToken) =>
                 {
                     Assert.Equal(TemplateId, request.TemplateId);
-                    Assert.Equal("{}", request.InitialResponseBody); // TODO check initial response body
+                    Assert.Equal("{}", request.InitialResponseBody);
                 })
                 .ReturnsAsync(createdApplication);
 
+            var expectedData = new Dictionary<string, object>
+            {
+                { "start-year", new { Value = "2026", Completed = true } },
+                { "end-year", new { Value = "2027", Completed = true } },
+                { "local-authority", new { Value = "LA1", Completed = true } }
+            };
+            string expectedJson = JsonSerializer.Serialize(expectedData, jsonSerializerOptions);
             ApplicationResponseDto applicationResponse = new(
                 Guid.Parse("00000000-0000-0000-0000-000000000002"),
                 createdApplication.ApplicationReference,
                 createdApplication.ApplicationId,
-                "{}",
+                string.Empty,
                 DateTime.UtcNow,
                 Guid.Parse("00000000-0000-0000-0000-000000000003")
             );
@@ -49,22 +64,27 @@ namespace GovUK.Dfe.FlexForms.Web.UnitTests.Services
                 .Callback<Guid, AddApplicationResponseRequest, CancellationToken>((applicationId, request, cancellationToken) =>
                 {
                     Assert.Equal(createdApplication.ApplicationId, applicationId);
-                    Assert.Equal("{}", request.ResponseBody); // TODO check response body
+                    byte[] data = Convert.FromBase64String(request.ResponseBody);
+                    string responseJson = Encoding.UTF8.GetString(data);
+                    output.WriteLine($"Response body: {Environment.NewLine}{responseJson}");
+                    Assert.Equal(expectedJson, responseJson);
                 })
                 .ReturnsAsync(applicationResponse);
 
             ApplicationDto submittedApplication = new()
             {
-                ApplicationId = createdApplication.ApplicationId
+                ApplicationId = createdApplication.ApplicationId, 
+                ApplicationReference = createdApplication.ApplicationReference,
+                Status = ApplicationStatus.Submitted
             };
             mockClient.Setup(c => c.SubmitApplicationAsync(createdApplication.ApplicationId))
                 .ReturnsAsync(submittedApplication);
 
             Dictionary<string, object> data = new()
             {
-                { "B1", "2026" },
-                { "B2", "2027" },
-                { "B3", "LA1" }
+                { "start-year", "2026" },
+                { "end-year", "2027" },
+                { "local-authority", "LA1" }
             };
 
             bool isSaved = await service.SaveApplicationAsync(ApplicationReference, data);
